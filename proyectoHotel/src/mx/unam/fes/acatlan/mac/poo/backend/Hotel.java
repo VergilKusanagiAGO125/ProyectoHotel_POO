1266c84817cce7a0cdb66edda30bc26cb4a68fac
VergilKusanagiAGO125 <149214648+VergilKusanagiAGO125@users.noreply.github.com>
2023-12-02 21:37:48 -0600

    Add files via upload

--- proyectoHotel/src/mx/unam/fes/acatlan/mac/poo/backend/Hotel.java
+++ proyectoHotel/src/mx/unam/fes/acatlan/mac/poo/backend/Hotel.java
@@ -1,3 +1,13 @@
+/* Proyecto Hotel POO
+ * 
+ * Integrentes:
+ * Castro Gonzalez Angel Gabriel
+ * Gabriel Cruz Alondra Paloma
+ * Gonzalez Ordaz Ariel
+ * Rosas Gonzalez Miguel Angel
+ * San Pedro Avila Pablo
+ */
+
 package mx.unam.fes.acatlan.mac.poo.backend;
 
 public class Hotel
